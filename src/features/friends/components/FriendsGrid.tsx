f68583c4a24fc36d553bba3265916302ce81531d
demna-miqava <demnamikava11@gmail.com>
2025-10-02 19:39:53 +0400

    add navigation to profile friends from the grid

--- src/features/friends/components/FriendsGrid.tsx
+++ src/features/friends/components/FriendsGrid.tsx
@@ -5,13 +5,17 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import FriendPopover from "./Popover";
+import { Link, useParams } from "react-router";
 
 const FriendsGrid = () => {
   const { friends, numberOfFriends } = useGetFriends();
+  const { userName } = useParams();
 
   return (
     <aside className="rounded-2xl border p-4 flex flex-col gap-4">
-      <h3 className="text-sm font-semibold">Friends ({numberOfFriends})</h3>
+      <Link to={`/profile/${userName}/friends`}>
+        <h3 className="text-sm font-semibold">Friends ({numberOfFriends})</h3>
+      </Link>
       <div className="grid grid-cols-5 gap-2">
         {friends.map((friend) => (
           <div key={friend.username} className="flex justify-center">
