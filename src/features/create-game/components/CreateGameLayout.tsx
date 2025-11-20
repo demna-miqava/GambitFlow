3abee53c9eb25a5f9d5836565624022e104b3400
demna-miqava <demnamikava11@gmail.com>
2025-11-20 17:26:51 +0400

    use layout in createGame

--- src/features/create-game/components/CreateGameLayout.tsx
+++ src/features/create-game/components/CreateGameLayout.tsx
@@ -1,16 +1,12 @@
 import { PlayBoard } from "./PlayBoard";
 import { CreateGameSidebar } from "./CreateGameSidebar";
 import { GameSetupProvider } from "@/features/create-game/GameSetupContext";
+import { GameLayout } from "@/features/game/components/GameLayout";
 
 export const CreateGameLayout = () => {
   return (
     <GameSetupProvider>
-      <div className="grid grid-cols-1 lg:grid-cols-[minmax(auto,800px)_auto] gap-6 p-4 lg:p-6 lg:justify-center">
-        <div className="hidden lg:block">
-          <PlayBoard />
-        </div>
-        <CreateGameSidebar />
-      </div>
+      <GameLayout board={<PlayBoard />} sidebar={<CreateGameSidebar />} />
     </GameSetupProvider>
   );
 };
