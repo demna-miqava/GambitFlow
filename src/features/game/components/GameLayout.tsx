7724b7a05c76784223fa8aa53863e7952821ec70
demna-miqava <demnamikava11@gmail.com>
2025-12-13 22:39:08 +0400

    update GameLayout styles

--- src/features/game/components/GameLayout.tsx
+++ src/features/game/components/GameLayout.tsx
@@ -7,7 +7,7 @@ interface GameLayoutProps {
 
 export const GameLayout = ({ board, sidebar }: GameLayoutProps) => {
   return (
-    <div className="h-full grid grid-cols-1 lg:overflow-hidden lg:grid-cols-[minmax(auto,680px)_400px] 2xl:grid-cols-[minmax(auto,800px)_350px] gap-6 lg:justify-center lg:content-center">
+    <div className="h-full grid grid-cols-1 lg:overflow-hidden lg:grid-cols-[minmax(auto,680px)_500px] 2xl:grid-cols-[minmax(auto,800px)_500px] gap-6 lg:justify-center lg:content-center">
       <div className="flex items-center justify-center">{board}</div>
       <div>{sidebar}</div>
     </div>
